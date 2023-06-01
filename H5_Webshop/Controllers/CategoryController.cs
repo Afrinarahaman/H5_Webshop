8fd868ee7378ec3809f606eab11268eab8f92c76
Afrinarahaman <70579511+Afrinarahaman@users.noreply.github.com>
2023-06-01 11:45:29 +0200

    Initial commit

---
+++ H5_Webshop/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace H5_Webshop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok("hello");
+        }
+    }
+}
